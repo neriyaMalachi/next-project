af1e547deea650abf86f29421c05009e252eba8b
Neriya Malachi <neriamalachi@gmail.com>
2024-11-07 08:05:22 +0200

    Test 2 deploy

--- src/app/api/apigroup/route.ts
+++ src/app/api/apigroup/route.ts
@@ -1,7 +1,6 @@
 import PostApi from "@/app/types/PostApi";
 import { NextResponse } from "next/server";
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-empty-object-type
 export function GET() {
   const posts: PostApi[] = Array.from({ length: 30 }).map((_, index) => {
     return {
--- src/app/pages/(ApiGroup)/apiUser/page.tsx
+++ src/app/pages/(ApiGroup)/apiUser/page.tsx
@@ -5,10 +5,10 @@ import PostApi from "@/app/types/PostApi";
 
 const page = async () => {
   const data = await postapi();
-  const posts: PostApi[] =data.posts
-
+  const posts: PostApi[] = data.data;
   return (
     <div>
+      <h1>API USERS</h1>
       {posts.map((user) => (
         <div className="" key={user.id}>
           <CardForApi id={user.id} body={user.body} title={user.title} />
