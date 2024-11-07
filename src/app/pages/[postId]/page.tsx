3e55727f2feb47e1def8e8d234334766a8b34d50
Neriya Malachi <neriamalachi@gmail.com>
2024-11-07 08:28:12 +0200

    Test 6 deploy

--- src/app/pages/[postId]/page.tsx
+++ src/app/pages/[postId]/page.tsx
@@ -4,14 +4,26 @@ import { getUser } from "@/app/services/userAction";
 // import PostView from "@/app/types/Post";
 import Link from "next/link";
 import React from "react";
+interface PostView {
+  params: {
+    postId: string;
+  };
+}
 
-const postPage = async (params:string) => {
-  const  postId = params;
+const postPage = async (params: PostView) => {
+  const { postId } = await params.params;
+  console.log("rrrr", postId);
+
   const { id, completed, todo, userId } = await getUser(postId);
   return (
     <div className="flex flex-col justify-evenly items-center h-[80vh] ">
       <Card_Todo id={id} completed={completed} todo={todo} userId={userId} />
-      <Link href={'/pages'} className="p-3 bg-red-500 rounded-md text-red-100 hover:bg-red-100 hover:text-red-500 " >חזור</Link>
+      <Link
+        href={"/pages"}
+        className="p-3 bg-red-500 rounded-md text-red-100 hover:bg-red-100 hover:text-red-500 "
+      >
+        חזור
+      </Link>
     </div>
   );
 };
