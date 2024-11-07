a674ab07d77c5a16900e628bac31a92a8aeec330
Neriya Malachi <neriamalachi@gmail.com>
2024-11-07 08:37:01 +0200

    Test 8 deploy

--- src/app/pages/post/[postId]/page.tsx
+++ src/app/pages/post/[postId]/page.tsx
@@ -1,26 +1,27 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import Card_Todo from "@/app/components/Card_Todo";
 import { getUser } from "@/app/services/userAction";
-// import PostView from "@/app/types/Post";
 import Link from "next/link";
 import React from "react";
-interface PostView {
+
+interface PostViewParams {
   params: {
     postId: string;
   };
 }
 
-const postPage = async (params: PostView) => {
-  const { postId } = await params.params;
-  console.log("rrrr", postId);
+const postPage = async ({ params }: PostViewParams) => {
+  const { postId } = params;
 
+  // משיג את המידע עבור הפוסט הספציפי
   const { id, completed, todo, userId } = await getUser(postId);
+
   return (
-    <div className="flex flex-col justify-evenly items-center h-[80vh] ">
+    <div className="flex flex-col justify-evenly items-center h-[80vh]">
       <Card_Todo id={id} completed={completed} todo={todo} userId={userId} />
       <Link
-        href={"/pages"}
-        className="p-3 bg-red-500 rounded-md text-red-100 hover:bg-red-100 hover:text-red-500 "
+        href="/pages"
+        className="p-3 bg-red-500 rounded-md text-red-100 hover:bg-red-100 hover:text-red-500"
       >
         חזור
       </Link>
