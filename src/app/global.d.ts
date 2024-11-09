bb0b95d0895089e6a45c8927006e75822607543a
Neriya Malachi <neriamalachi@gmail.com>
2024-11-09 21:14:11 +0200

    text 3 neriya

--- src/app/global.d.ts
+++
@@ -1,7 +0,0 @@
-// src/global.d.ts
-
-// import mongoose from 'mongoose';
-
-// declare global {
-//   let mongoose: { conn: typeof mongoose | null; promise: Promise<typeof mongoose> | null };
-// }
